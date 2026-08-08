7ce5ee66b7f0318784a4f617828af2844fb7c11a
syed roshan <toroshaninbox1@gmail.com>
2026-08-08 21:02:01 +0530

    Make Socket.IO client use VITE_API_BASE if set

--- renderer/src/socket.js
+++ renderer/src/socket.js
@@ -1,6 +1,8 @@
 import { io } from 'socket.io-client';
 
-const socket = io(window.location.origin, {
+const API_BASE = import.meta.env.VITE_API_BASE || window.location.origin;
+
+const socket = io(API_BASE, {
     path: '/socket.io',
     transports: ['websocket', 'polling'],
     reconnectionAttempts: 10,
